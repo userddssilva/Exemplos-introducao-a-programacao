33119bdfdcc8eb60baba005e2a53c881e30ac32c
Dayvson Silva <userddssilva@gmail.com>
2021-03-19 11:41:36 -0400

    refactor: atualiza código de recução de fibonacci

--- recucao.py
+++ recucao.py
@@ -1,19 +1,18 @@
-# 1, 1, 2, 3, 5, 8, 13
+# fibonacci: 1, 1, 2, 3, 5, 8, 13, ...
 
-fibonacci(6)
-
-                        8 fib(6)
-                    /                \ 
-             5  fib(5)               3    fib(4)
-             /       \                /           \
-    3    fib(4)     2 fib(3)          2 fib(3)     1 fib(2)
-        /  \        /     \           /    \            /
-2    fib(3) 1 fib(2) 1 fib(2)   1 fib(1)  1 fib(2)    1
-    /  \      \      /       /        /      \ 
-2 fib(2) fib(1) 1     1       1        1     1
-  /       \
-  1       1
-               
+"""
+                           fib(5)
+                        /          \
+                     fib(4)        fib(3)
+                    /    \         /   \
+                 fib(2) fib(3)  fib(1) fib(2)
+                /       /    \      \      \
+            fib(1)    fib(1) fib(2)  1     fib(1)
+            /          /        \           /
+            1         1       fib(1)       1 
+                               /
+                              1
+"""
 
 def fibonacci(n):
     if n == 1:
@@ -23,3 +22,6 @@ def fibonacci(n):
     else:
         return fibonacci(n-1) + fibonacci(n-2)
         
+
+if __name__ == "__main__" :
+    print(fibonacci(5))
